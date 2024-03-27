69a2beed396548f55ec3d9e048b8ee0cfb34b087
Timothy Barasa <timbaros87@gmail.com>
2024-03-27 22:50:09 +0300

    Adds arrow functions

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// Define a function called divide that takes two parameters, a and b
+function divide(a, b) {
+    return a / b; // Return the result of dividing the first parameter (a) by the second parameter (b)
+} 
+console.log(divide(200, 100)); // Call the divide function with arguments 200 and 100, and log the result to the console
+
+
+// Define an arrow function called square that takes one parameter, c, and returns the square of c
+const square = (c) => c * c;
+console.log(square(2)); // Call the square function with argument 2 and log the result to the console
+
+// Define an arrow function called add that takes two parameters, d and e, and returns their sum
+const add = (d, e) => d + e;
+console.log(add(3, 4)); // Call the add function with arguments 3 and 4, and log the result to the console
+
